7475e7cbc75166fbf8cfa20b5765cf9a42c4a0c4
ValentinaArdilaPerez <valentina.ardilap@udea.edu.co>
2023-04-20 19:26:51 -0500

    Continue modulo 2 y ahi cierro el codigo

--- codigo/main.cpp
+++ codigo/main.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+void crear_HORARIO(int filas, int columnas,int aux,char HORARIO[][8]);
+void separar_horaio_materia(char horario_materia[6],char &columna, char &fila,char HORARIO[][8],int filas, int columnas);
+
 int main() {
     ifstream archivo;
     archivo.open("pensum.txt");
@@ -77,6 +80,8 @@ int main() {
            cout << "Horario: " << horario << endl;
             } //extrae horario
     }
+    archivo.close();
+
     char HORARIO[][8] = {
         'H', 'L', 'M', 'W', 'J', 'V', 'S', 'D',
         '6', '-', '-', '-', '-', '-', '-', '-',
@@ -92,82 +97,131 @@ int main() {
         '1', '-', '-', '-', '-', '-', '-', '-',
         '1', '-', '-', '-', '-', '-', '-', '-',
         '1', '-', '-', '-', '-', '-', '-', '-',
-        '2', '-', '-', '-', '-', '-', '-', '-',
+        '1', '-', '-', '-', '-', '-', '-', '-',
         '2', '-', '-', '-', '-', '-', '-', '-',
         '2', '-', '-', '-', '-', '-', '-', '-'
     };
 
     int filas = (sizeof(HORARIO)/sizeof(HORARIO[0]));
     int columnas = (sizeof(HORARIO[0])/sizeof(HORARIO[0][0]));
-    int aux=1;
-    // Iterar sobre cada fila y columna de la matriz
-    for (int i = 0; i < filas; i++){
-        for (int j = 0; j < columnas; j++){
-            if (i>=5 && i<=13 && j==0){
-                cout<<HORARIO[i][j]<< aux <<" | ";
-                aux++;
-                continue;
-            }
-            if (aux==10){
-                aux=0;
-            }
-            if (i>=14 && i<=17 && j==0){
-                cout<<HORARIO[i][j]<< aux <<" | ";
-                aux++;
-                continue;
-            }
-            cout<<HORARIO[i][j]<<"  | ";
-        }//cierra for columna
-        cout << endl;
-    }//cierra for fila
+    int aux=0;
 
+    //crear_HORARIO(filas,columnas,aux,HORARIO);
 
     // Ubicar la materia en la matriz
-    char columna;
+    char columna,fila;
     char horario_materia[6];
 
     cin >> horario_materia;
     //cout << horario_materia<< endl;
 
-    if (horario_materia[0]=='L')
-        columna=1;
-    if (horario_materia[0]=='M')
-        columna=2;
-    if (horario_materia[0]=='W')
-        columna=3;
-    if (horario_materia[0]=='J')
-        columna=4;
-    if (horario_materia[0]=='V')
-        columna=5;
-    if (horario_materia[0]=='S')
-        columna=6;
+    separar_horaio_materia(horario_materia,columna,fila,HORARIO,filas,columnas);
 
-    HORARIO[3][columna]='hY';
-
-
-    aux=1;
-    // Iterar sobre cada fila y columna de la matriz
-    for (int i = 0; i < filas; i++){
-        for (int j = 0; j < columnas; j++){
-            if (i>=5 && i<=13 && j==0){
-                cout<<HORARIO[i][j]<< aux <<" | ";
-                aux++;
-                continue;
-            }
-            if (aux==10){
-                aux=0;
-            }
-            if (i>=14 && i<=17 && j==0){
-                cout<<HORARIO[i][j]<< aux <<" | ";
-                aux++;
-                continue;
-            }
-            cout<<HORARIO[i][j]<<"  | ";
-        }//cierra for columna
-        cout << endl;
-    }//cierra for fila
-
-    archivo.close();
-
+    aux=0;
+    crear_HORARIO(filas,columnas,aux,HORARIO);
     return 0;
+}
+
+void crear_HORARIO(int filas, int columnas,int aux,char HORARIO[][8]){
+for (int i = 0; i < filas; i++){
+    for (int j = 0; j < columnas; j++){
+        if (i>=5 && i<=14 && j==0){
+            cout<<HORARIO[i][j]<< aux <<" | ";
+            aux++;
+            continue;
+        }
+        if (aux==10){
+            aux=0;
+        }
+        if (i>=14 && i<=17 && j==0){
+            cout<<HORARIO[i][j]<< aux <<" | ";
+            aux++;
+            continue;
+        }
+        cout<<HORARIO[i][j]<<"  | ";
+    }//cierra for columna
+    cout << endl;
+}//cierra for fila
+
+}
+void separar_horaio_materia(char horario_materia[6],char &columna, char &fila,char HORARIO[][8],int filas, int columnas){
+    char a_columnas[6]={'L','M','W','J','V','S'};
+    char a_filas[10]={'0','1','2','3','4','5','6','7','8','9'};
+    int hora1;
+    for (char var = 0; var < 6; var++) {
+        if (horario_materia[0]==a_columnas[var])
+            columna=var+1;//COLUMNA +1 Por que la columna 0 es la de el horario
+    }
+    for (char var = 0; var < 10; var++) {
+        if (horario_materia[1]==a_filas[0]){
+            for (char i = 0; i < 4; i++) { // 4 por que es solo  6 - 7 - 8 - 9
+                if (horario_materia[2]==a_filas[i+6]){ // +6 por que i esta ubicado en la posicion 0 de a_filas y el horario empieza a las 6am
+                    fila=i+1; // +1 por que la fila 0 es la de el horario
+                    hora1=fila;
+                    HORARIO[fila][columna]='Y';
+                    break;
+                }
+            }
+        }
+        if (horario_materia[1]==a_filas[1]){
+            for (char i = 0; i < 10; i++) { // 4 por que es solo  6 - 7 - 8 - 9
+                if (horario_materia[2]==a_filas[i]){ // +6 por que i esta ubicado en la posicion 0 de a_filas y el horario empieza a las 6am
+                    fila=i+5; // +5 por que la fila 0 es la de el horario y te la 1 a la 4 son de 6am a 9am, las 10am esta es la posicion 5
+                    hora1=fila;
+                    HORARIO[fila][columna]='Y';
+                    break;
+                }
+            }
+        }
+        if (horario_materia[1]==a_filas[2]){
+            for (char i = 0; i < 2; i++) { // 4 por que es solo  6 - 7 - 8 - 9
+                if (horario_materia[2]==a_filas[i]){ // +6 por que i esta ubicado en la posicion 0 de a_filas y el horario empieza a las 6am
+                    fila=i+15; // +15 por que las 20pm esta es la posicion 15
+                    hora1=fila;
+                    HORARIO[fila][columna]='Y';
+                    break;
+                }
+            }
+        }
+        /*int aux=0;
+        crear_HORARIO(filas,columnas,aux,HORARIO);*/
+        if (horario_materia[4]==a_filas[0]){
+            for (char i = 0; i < 4; i++) { // 4 por que es solo  6 - 7 - 8 - 9
+                if (horario_materia[5]==a_filas[i+6]){ // +6 por que i esta ubicado en la posicion 0 de a_filas y el horario empieza a las 6am
+                    fila=i+1; // +1 por que la fila 0 es la de el horario
+                    while (fila>hora1){
+                        HORARIO[fila][columna]='Y';
+                        fila--;
+                    }
+                    break;
+                }
+            }
+        }
+        if (horario_materia[4]==a_filas[1]){
+            for (char i = 0; i < 10; i++) { // 4 por que es solo  6 - 7 - 8 - 9
+                if (horario_materia[5]==a_filas[i]){ // +6 por que i esta ubicado en la posicion 0 de a_filas y el horario empieza a las 6am
+                    fila=i+5; // +5 por que la fila 0 es la de el horario y te la 1 a la 4 son de 6am a 9am, las 10am esta es la posicion 5
+                    while (fila>hora1){
+                        HORARIO[fila][columna]='Y';
+                        fila--;
+                    }
+                    break;
+                }
+            }
+        }
+        if (horario_materia[4]==a_filas[2]){
+            for (char i = 0; i < 2; i++) { // 4 por que es solo  6 - 7 - 8 - 9
+                if (horario_materia[5]==a_filas[i]){ // +6 por que i esta ubicado en la posicion 0 de a_filas y el horario empieza a las 6am
+                    fila=i+15; // +15 por que las 20pm esta es la posicion 15
+                    while (fila>hora1){
+                        HORARIO[fila][columna]='Y';
+                        fila--;
+                    }
+                    break;
+                }
+            }
+        }
+        /*aux=0;
+        crear_HORARIO(filas,columnas,aux,HORARIO);*/
+    }
 }
